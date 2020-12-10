015d212b03e60673da9087afdcb22b10cf975fac
cc <jycdeya@gmail.com>
2020-12-10 20:46:10 +0800

    chap7

---
+++ chapter7/704.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<cctype>
+#include<stdexcept>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+struct Person{
+	string name;
+	string address;	
+};
+int main(){
+	
+}
---
+++ chapter7/706.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+struct Sales_data{
+	string book_id;
+	string book_name;
+	unsigned units_sold;
+	double book_price = 0.0;
+	double revenue = 0.0;
+	string isbn() const {return this->book_id;}
+	Sales_data &combine(Sales_data rhs);	
+};
+Sales_data& Sales_data::combine(Sales_data rhs){
+	this->units_sold+=rhs.units_sold;
+	this->revenue+=rhs.revenue;
+	return *this;
+}
+Sales_data add(const Sales_data a,const Sales_data b){
+	Sales_data newObj = a;
+	newObj.combine(b);
+	return newObj;
+}
+std::istream &read(std::istream &is,Sales_data &Obj){
+	is>>Obj.book_id>>Obj.book_name>>Obj.units_sold>>Obj.book_price;
+	Obj.revenue = Obj.units_sold*Obj.book_price;
+	return is;
+}
+std::ostream &print(std::ostream &os,const Sales_data &Obj){
+	os<<"book isbn:"<<Obj.book_id<<" "<<" book name:"<<Obj.book_name<<" book sold:"<<Obj.units_sold<<" book revenue:"<<Obj.revenue;
+	return os;
+}
+
+
+int main(){
+	Sales_data test1,test2;
+	read(cin,test1);
+	read(cin,test2);
+	Sales_data sum = add(test1,test2);
+	print(cout,sum);
+	cout<<endl;
+	return 0;
+}
