b329af173a20b0ad3fe735ea3277359feea787f0
cc <jycdeya@gmail.com>
2020-12-12 17:57:43 +0800

    chap7

--- chapter7/ccScreen.h
+++ chapter7/ccScreen.h
@@ -2,8 +2,18 @@
 #include<iostream>
 #include <ostream>
 #include<string>
+#include<vector>
 #ifndef exp723
 #define exp723
+class Screen;
+class Window_mgr{
+public:
+	using PageIndex = std::string::size_type;
+	inline void clear(PageIndex i);
+private:
+	std::vector<Screen> screens;
+};
+
 class Screen{
 	public:
 	       	using pos = std::string::size_type;
@@ -18,16 +28,22 @@ class Screen{
 		Screen &display(std::ostream &os){ do_display(os); return *this; }
 		const Screen &display(std::ostream &os)const{ do_display(os); return *this; }
 	private:
+		friend void Window_mgr::clear(PageIndex);
 		pos Height = 0 ,Width = 0;
 		pos cursor = 0;
 		std::string content;
 		inline void do_display (std::ostream &os)const;
 };
-inline Screen& Screen::set(char ch){
+inline void Window_mgr::clear(PageIndex i){
+	Screen &s = screens[i];
+	s.content = std::string(s.Height*s.Width,' ');
+}
+
+inline Screen &Screen::set(char ch){
 	content[cursor] = ch;
 	return *this;
 }
-inline Screen& Screen::set(pos row,pos col,char ch){
+inline Screen &Screen::set(pos row,pos col,char ch){
 	content[row*Width+col] = ch;
 	return *this;
 }
